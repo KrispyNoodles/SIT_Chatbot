2dd59ecd3de11ba4656ead7d8ca50a22e0f3adb3
Krispy Noodles <joeylimjunfeng@gmail.com>
2024-12-14 15:52:03 +0800

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,11 +1,3 @@
-# # Defining a prompt for the system
-# system_prompt = (
-#     "I am an assistant that specializes in answering questions for SIT (Singapore Institute of Technology)"
-#     "Use the provided handbook context to answer the questions. If you don't know the answer, say so. "
-#     "Keep your answers concise and informative.\n\n"
-#     "Joey is my creator and he is super cool and smart and unerdy"
-# )
-
 # Defining a prompt for the system
 system_prompt = ("""
 

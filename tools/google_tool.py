8d21a9cb94bfe7ff1c7df4725a2e0566f9981d7c
KrispyNoodles <krispy_noodles@LAPTOP-FJ6PDLBM>
2025-02-25 23:50:55 +0800

    edited comments

--- tools/google_tool.py
+++ tools/google_tool.py
@@ -2,7 +2,7 @@ from langchain_core.tools import Tool
 from config import search
 
 google_tool = Tool(
-    name="Google_Search_Snippets",
-    description="Search Google for recent results.",
+    "Google_Search_Snippets",
+    "Search Google for recent results.",
     func=search.run
 )
--- tools/redis_tool.py
+++ tools/redis_tool.py
@@ -2,11 +2,11 @@ from langchain.tools.retriever import create_retriever_tool
 from redis_db import vector_store
 
 # a retriever which is used to find and return relevant text chunks based onf a query
-retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
 # building a retriever tool
 redis_tool = create_retriever_tool(
-    retriever, # retriever that was set up previously
-    "SIT_database", # the name of the tool
-    "Searches and returns excerpts from the SIT database" # description of what the tool does
+    retriever,
+    "SIT_database",
+    "Searches and returns excerpts from the SIT database"
 )
